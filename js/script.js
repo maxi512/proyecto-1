9702e56b5c026539f76a76fd9412d238d18f3690
maxi512 <mampuyol512@gmail.com>
2020-04-21 17:41:25 -0300

    Cambie deteccion de caracteres en el input

--- js/script.js
+++ js/script.js
@@ -1,33 +1,5 @@
 var longUlt = [];
 
-function detectar() {
-    texto = document.getElementById("texto").value;
-    var key = event.keyCode || event.charCode;
-    textoAux = document.getElementById("ascii").innerHTML;
-
-    if (texto != "") {
-        //se puede acomodar
-        if (key == 8) {
-            pop = longUlt.pop()
-            var res = textoAux.substring(0, textoAux.length - pop);
-            document.getElementById("ascii").innerHTML = res;
-
-        } else {
-            if (event.key.length == 1) {
-                var mostrar = texto.charCodeAt(texto.length - 1) + "";
-                document.getElementById("ascii").innerHTML = document.getElementById("ascii").innerHTML + " " + mostrar;
-                longUlt.push(parseInt(mostrar.length + 1));
-            }
-        }
-
-    } else {
-        document.getElementById("ascii").innerHTML = "";
-    }
-
-
-
-}
-
 function mostrarSeleccion() {
     texto = window.getSelection().toString();
     if (texto != "") {
@@ -41,4 +13,43 @@ function ocultar() {
 
 function mostrar() {
     document.getElementById("jumbotron").style.display = "block";
+}
+
+
+function myKeyPress(e) {
+    var keynum;
+
+    if (window.event) { // IE                    
+        keynum = e.keyCode;
+    } else if (e.which) { // Netscape/Firefox/Opera                   
+        keynum = e.which;
+    }
+
+    texto = document.getElementById("texto").value;
+    textoAux = document.getElementById("ascii").innerHTML;
+
+    console.log("El key num es: " + keynum + "y es de tipo:" + typeof(keynum));
+
+    if (event.key.length == 1) {
+        document.getElementById("ascii").innerHTML = textoAux + " " + keynum;
+
+        console.log("Puse en pila: " + parseInt(keynum.toString().length + 1));
+        longUlt.push(parseInt(keynum.toString().length + 1));
+    }
+
+}
+
+//Detecto backspace
+//Falta detectar supr
+function myKeyDown() {
+    var key = event.keyCode || event.charCode;
+
+    textoAux = document.getElementById("ascii").innerHTML;
+
+    if (key == 8) {
+        pop = longUlt.pop();
+        console.log("Saque de pila: " + pop);
+        var res = textoAux.substring(0, textoAux.length - pop);
+        document.getElementById("ascii").innerHTML = res;
+    }
 }
