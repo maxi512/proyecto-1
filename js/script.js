441e6114b08bff8f7edfb0d898a34daf60addf3b
maxi512 <mampuyol512@gmail.com>
2020-04-23 15:51:42 -0300

    Añadi conversion a valores unicode

--- js/script.js
+++ js/script.js
@@ -1,55 +1,94 @@
-var longUlt = [];
+var lastString = "";
 
+/**
+ * Accion al seleccionar: Pendiente
+ */
 function mostrarSeleccion() {
     texto = window.getSelection().toString();
     if (texto != "") {
-        alert(texto);
+
     }
 }
-
+/**
+ * Oculta Jumbotron
+ */
 function ocultar() {
     document.getElementById("jumbotron").style.display = "none";
 }
 
+/**
+ * Muestra Jumbotron
+ */
 function mostrar() {
     document.getElementById("jumbotron").style.display = "block";
 }
 
-
-function myKeyPress(e) {
-    var keynum;
-
-    if (window.event) { // IE                    
-        keynum = e.keyCode;
-    } else if (e.which) { // Netscape/Firefox/Opera                   
-        keynum = e.which;
+/**
+ * Convierte una cadena en sus valores unicode
+ * @param {String} texto del cual se quiere saber sus valores unicode
+ */
+function calcularCodigosUnicode(texto) {
+    var output = "";
+    var pair = "";
+    var i = 0;
+    while (i < texto.length) {
+        codePoint = texto.codePointAt(i);
+        add = "";
+        //Es surrogate pair?
+        if (codePoint >= 55296) {
+            //Armo texto con surrogate pair
+            pair = texto.charAt(i) + texto.charAt(i + 1) + "";
+            add = pair.charCodeUTF32();
+            //Salteo surrogate trail
+            i++;
+        } else {
+            add = codePoint;
+        }
+        output += "U+" + add.toString(16) + " ";
+        i++;
     }
 
+    lastString = output.toUpperCase();
+    return lastString;
+}
+
+/**
+ * Verifica si hay que modificar el texto
+ */
+function onModified1() {
     texto = document.getElementById("texto").value;
-    textoAux = document.getElementById("ascii").innerHTML;
 
-    console.log("El key num es: " + keynum + "y es de tipo:" + typeof(keynum));
-
-    if (event.key.length == 1) {
-        document.getElementById("ascii").innerHTML = textoAux + " " + keynum;
-
-        console.log("Puse en pila: " + parseInt(keynum.toString().length + 1));
-        longUlt.push(parseInt(keynum.toString().length + 1));
+    if (texto != lastString) {
+        document.getElementById("ascii").innerHTML = calcularCodigosUnicode(texto);
     }
 
 }
 
-//Detecto backspace
-//Falta detectar supr
+//Espera a que se pueda leer el texto
+function onModified() {
+    setTimeout(onModified1, 0);
+}
+
+/**
+ * verifica si hay que modificar el texto al presionar supr o backspace
+ */
 function myKeyDown() {
     var key = event.keyCode || event.charCode;
-
-    textoAux = document.getElementById("ascii").innerHTML;
-
-    if (key == 8) {
-        pop = longUlt.pop();
-        console.log("Saque de pila: " + pop);
-        var res = textoAux.substring(0, textoAux.length - pop);
-        document.getElementById("ascii").innerHTML = res;
+    if (key == 8 || key == 46) {
+        onModified();
     }
+}
+
+/**
+ * Obtiene Unicode de surrogate pairs
+ */
+String.prototype.charCodeUTF32 = function() {
+    return ((((this.charCodeAt(0) - 0xD800) * 0x400) + (this.charCodeAt(1) - 0xDC00) + 0x10000));
+};
+/**
+ * Action on paste
+ */
+function myPaste() {
+    setTimeout(onModified1, 0);
+
 }
