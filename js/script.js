d88f1dc25e7e98aaf435dc95d0eff44040e4a870
maxi512 <mampuyol512@gmail.com>
2020-04-29 22:50:12 -0300

    Agregue manejo de cadenas en local storage

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
-var state = ""
+var state = "";
+populateDropdown();
+
+function mostrarSeleccion() {
+    texto = window.getSelection().toString();
+    if (texto != "") {
+
+    }
+}
+
+
+///////////////////////////////Jumbotron////////////////////////////////
+/**
+ * Verifica si hay que mostrar jumbotron
+ */
+function actualizar() {
+    checkbox = document.getElementById("checkbox");
+    if (checkbox.checked) {
+        localStorage.setItem("mostrarJumbotron", "false");
+    } else {
+        localStorage.setItem("mostrarJumbotron", "true");
+    }
+}
 
 /**
  * Accion al seleccionar: Pendiente
@@ -12,12 +34,6 @@ function verificarMostrarJumbotron() {
     }
 }
 
-function mostrarSeleccion() {
-    texto = window.getSelection().toString();
-    if (texto != "") {
-
-    }
-}
 /**
  * Oculta Jumbotron
  */
@@ -34,6 +50,10 @@ function mostrarJumbotron() {
     document.getElementById("separador").classList.remove('sr-only');
 }
 
+//////////////////////////////////////////////////////////////
+
+
+///////////////////////////////Codificaciones////////////////////////////////
 /**
  * Convierte una cadena en sus valores unicode
  * @param {String} texto del cual se quiere saber sus valores unicode
@@ -86,7 +106,9 @@ function calcularCodigosAscii(texto) {
     return output;
 
 }
+///////////////////////////////Codificaciones////////////////////////////////
 
+///////////////////////////////Actualiza textos////////////////////////////////
 /**
  * Modifica la salida de acuerdo al texto ingresado y la codificacion seleccionada
  */
@@ -112,7 +134,7 @@ function onModified() {
  * verifica si hay que modificar el texto al presionar supr o backspace
  */
 function myKeyDown() {
-    var key = event.keyCode || event.charCode;
+    const key = event.keyCode || event.charCode;
     if (key == 8 || key == 46) {
         onModified();
     }
@@ -133,17 +155,9 @@ function myPaste() {
 
 }
 
-/**
- * Verifica si hay que mostrar jumbotron
- */
-function actualizar() {
-    checkbox = document.getElementById("checkbox");
-    if (checkbox.checked) {
-        localStorage.setItem("mostrarJumbotron", "false");
-    } else {
-        localStorage.setItem("mostrarJumbotron", "true");
-    }
-}
+///////////////////////////////Actualiza textos////////////////////////////////
+
+/////////////////////////////////ACtualiza states/////////////////////////////
 
 function setStateAscii() {
     state = "Ascii"
@@ -154,3 +168,99 @@ function setStateUnicode() {
     state = "Unicode"
     onModified1();
 }
+
+/////////////////////////////////Actualiza states/////////////////////////////
+
+////////////////////////////////Storage managment/////////////////////////////
+
+/**
+ * Almacena en el local storage la cadena actual y lo añade al dropdown menu correspondiente
+ */
+function addUserInput() {
+    const toStore = document.getElementById("texto").value;
+    const dropdown = document.getElementById("dropdownStorage");
+
+    //Solo guardo si el input no es igual al ultimo guardado y no es vacio
+    if (toStore != dropdown.firstChild.textContent && toStore !== "") {
+        //obtengo guardadas
+        const storedInputs = getStoredInputs();
+        ///añadpoal arreglo
+        addToArray(storedInputs, toStore);
+        //guardo
+        localStorage.setItem("userInputs", JSON.stringify(storedInputs));
+        //actualizo dropdown
+        var newItem = createDropdownItem(toStore)
+        dropdown.prepend(newItem);
+    }
+
+}
+/**
+ * obtiene las ultimas cadenas guardadas del local storage
+ */
+function getStoredInputs() {
+    let storedInputs = JSON.parse(localStorage.getItem("userInputs"));
+    if (storedInputs == null)
+        storedInputs = [];
+    return storedInputs;
+}
+
+/**
+ * Añade a un arreglo una cadena de caracteres, si el tamaño se agranda por encima
+ * del limite, hace un shift
+ * @param {*} arr array al que se le quiere agregar una cadena
+ * @param {String} toStore cadena que se quiere almacenar
+ */
+function addToArray(arr, toStore) {
+    arr.push(toStore);
+    if (arr.length > 5) {
+        arr.shift();
+        removeLastElementInDropdown();
+    }
+
+}
+
+/**
+ * Llena el dropdown menu a partir de lo que esta guardado en el local storage
+ */
+function populateDropdown() {
+    let dropdown = document.getElementById("dropdownStorage");
+    const storedInputs = getStoredInputs();
+    for (let i = 0; i < storedInputs.length; i++) {
+        const inputStored = storedInputs[i];
+        const dropdownItem = createDropdownItem(inputStored);
+        dropdown.prepend(dropdownItem);
+    }
+}
+
+/**
+ * Crea y devuelve un dropdown-item con el comportamiento deseado
+ * @param {String} text que se desea usar como textContent del item creado 
+ */
+function createDropdownItem(text) {
+    let newItem = document.createElement("a");
+    newItem.classList.add("dropdown-item");
+    newItem.href = "#";
+    newItem.textContent = text;
+    newItem.addEventListener('click', onClickDropdownItem)
+    return newItem;
+}
+
+/**
+ * Remueve el ultimo elemento del dropdown menu que contiene las cadenas en local storage
+ */
+function removeLastElementInDropdown() {
+    const dropdown = document.getElementById("dropdownStorage");
+    dropdown.removeChild(dropdown.lastChild);
+}
+
+/**
+ * Accion al clickear sobre un dropdown-item:
+ * Setear el texto del input con su textContent
+ */
+function onClickDropdownItem() {
+    const text = this.textContent;
+    const input = document.getElementById('texto');
+    input.value = text;
+    onModified1();
+}
+////////////////////////////////Storage managment/////////////////////////////
